debb214bf6bd2b806f3a6854aeae8df75072261e
OleksiyDer <leha.aderibo@yandex.ru>
2023-11-21 00:10:49 +0700

    all ok!

--- app/src/main/java/com/example/conwayslife/MainActivity.java
+++ app/src/main/java/com/example/conwayslife/MainActivity.java
@@ -2,26 +2,38 @@ package com.example.conwayslife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int valueSize = 5;
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SeekBar seekBar = findViewById(R.id.seekBar);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(String.valueOf(seekBar.getProgress()));
+
+        Button buttonCreate = (Button) findViewById(R.id.button_create);
+
+        TextView size = (TextView)findViewById(R.id.size);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+
+        Button buttonStart = (Button) findViewById(R.id.button_start);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                textView.setText(String.valueOf(progress));
+                size.setText(String.valueOf(progress));
+                valueSize = progress;
             }
 
             @Override
@@ -34,8 +46,61 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawField(valueSize);
+            }
+        });
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
+
     }
 
+    private void start() {
 
 
+    }
+
+    public void drawField(int size) {
+
+        LinearLayout linearLayoutMain = findViewById(R.id.mainlayout);
+        linearLayoutMain.removeAllViews();
+
+        button_cell[][] button_cell = new button_cell[size][size];
+
+
+        for (int x = 0; x < size; x++){
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            linearLayout.setLayoutParams(layoutParams);
+            linearLayoutMain.addView(linearLayout);
+            for (int y = 0; y < size; y++){
+                Button button = new Button(this);
+                LinearLayout.LayoutParams layoutParamsButton = new LinearLayout.LayoutParams
+                        (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                button.setLayoutParams(layoutParamsButton);
+                button.setTag(x + "*" + y);
+                button_cell[x][y] = new button_cell();
+                button_cell[x][y].set_id(x, y);
+                button.setOnClickListener(this);
+                linearLayout.addView(button);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View button) {
+        TextView size = (TextView)findViewById(R.id.size);
+        size.setText(button.getTag().toString());
+
+    }
 }
--- app/src/main/java/com/example/conwayslife/button_cell.java
+++ app/src/main/java/com/example/conwayslife/button_cell.java
@@ -2,12 +2,17 @@ package com.example.conwayslife;
 
 public class button_cell {
     boolean condition;
-
+    int id_x;
+    int id_y;
     public button_cell() {
         this.condition = false;
+        this.id_x = 0;
+        this.id_y = 0;
     }
-    public button_cell(boolean condition) {
+    public button_cell(boolean condition, int x, int y) {
         this.condition = condition;
+        this.id_x = x;
+        this.id_y = y;
     }
 
     public boolean get_condition() {
@@ -16,5 +21,17 @@ public class button_cell {
 
     public void set_condition(boolean condition) {
         this.condition = condition;
+    }
+
+    public int[] get_id() {
+        this.id_y = id_x;
+        this.id_y = id_y;
+        int [] id = {id_x, id_y};
+        return id;
+    }
+
+    public void set_id(int id_x, int id_y) {
+        this.id_x = id_x;
+        this.id_y = id_y;
     }
 }
