3258966cde08eaa74383106bf043bdcbdb07ca26
OleksiyDer <leha.aderibo@yandex.ru>
2023-12-09 19:23:20 +0700

    1

--- app/src/main/java/com/example/conwayslife/MainActivity.java
+++ app/src/main/java/com/example/conwayslife/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     int valueSize = 5;
 
-    button_cell[][] button_cell;
-    button_cell[] Life_button_cell;
+    button_cell[][] first_generation;
+
+    button_cell[][] next_generation;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +52,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
         buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,17 +66,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                start();
-            }
-        });
+
 
     }
 
     private void start() {
+        int n = first_generation.length;
+        int m = first_generation[0].length;
 
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (first_generation[i][j].get_condition() == true){
+                    next_generation[i][j].set_condition(true);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int aliveNeighbors = countAliveNeighbors(first_generation, i, j);
+
+                 if (next_generation[i][j].get_condition() == true) {
+                    if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+                        next_generation[i][j].set_condition(false); // клетка умирает
+                    } else {
+                        next_generation[i][j].set_condition(true); // клетка остается живой
+                    }
+                } else {
+                    if (aliveNeighbors == 3) {
+                        next_generation[i][j].set_condition(true); // клетка оживает
+                    }
+                }
+            }
+        }
+        drawField(next_generation);
 
     }
 
@@ -78,9 +109,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         LinearLayout linearLayoutMain = findViewById(R.id.mainlayout);
         linearLayoutMain.removeAllViews();
-        button_cell = new button_cell[size][size];
-
-        Life_button_cell = new button_cell[size*size];
+        first_generation = new button_cell[size][size];
+        next_generation = new button_cell[size][size];
 
         for (int x = 0; x < size; x++){
             LinearLayout linearLayout = new LinearLayout(this);
@@ -96,8 +126,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 button.setLayoutParams(layoutParamsButton);
                 button.setTag(x + " " + y);
                 button.setBackgroundResource(R.drawable.button_false);
-                button_cell[x][y] = new button_cell();
-                button_cell[x][y].set_id(x, y);
+                first_generation[x][y] = new button_cell();
+                next_generation[x][y] = new button_cell();
                 button.setOnClickListener(this);
                 linearLayout.addView(button);
             }
@@ -111,14 +141,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int x = Integer.parseInt(XY[0]);
 
         int y = Integer.parseInt(XY[1]);
-
-        button_cell[x][y].reverse_condition();
-        if (button_cell[x][y].get_condition() == true) {
+        
+        first_generation[x][y].reverse_condition();
+        if (first_generation[x][y].get_condition() == true) {
             button.setBackgroundResource(R.drawable.button_true);
         }
         else {
             button.setBackgroundResource(R.drawable.button_false);
         }
 
+    }
+
+    public int countAliveNeighbors(button_cell[][] button_cell, int x, int y) {
+        int count = 0;
+        int index = 0;
+
+        if (button_cell[x][y].get_condition() == true){
+            index = 1;
+        }
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                // Проверяем, не выходит ли индекс за пределы массива
+                if (i >= 0 && i < button_cell.length && j >= 0 && j < button_cell[i].length) {
+                    // Если текущая ячейка содержит цифру "1"
+                    if (button_cell[i][j].get_condition() == true) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count - index;
+    }
+
+    public void drawField(button_cell[][] button_cell) {
+        LinearLayout linearLayoutMain = findViewById(R.id.mainlayout);
+        linearLayoutMain.removeAllViews();
+        for (int x = 0; x < button_cell.length; x++){
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            linearLayout.setLayoutParams(layoutParams);
+            linearLayoutMain.addView(linearLayout);
+            for (int y = 0; y < button_cell.length; y++){
+                Button button = new Button(this);
+                LinearLayout.LayoutParams layoutParamsButton = new LinearLayout.LayoutParams
+                        (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                button.setLayoutParams(layoutParamsButton);
+                if (button_cell[x][y].get_condition() == true){
+                    button.setBackgroundResource(R.drawable.button_true);
+                }
+                else {
+                    button.setBackgroundResource(R.drawable.button_false);
+                }
+
+                linearLayout.addView(button);
+            }
+        }
+
+        for (int i = 0; i < button_cell.length; i++){
+            for (int j = 0; j < button_cell.length; j++){
+                if (next_generation[i][j].get_condition() == true){
+                    first_generation[i][j].set_condition(true);
+                }
+                else {
+                    first_generation[i][j].set_condition(false);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/conwayslife/button_cell.java
+++ app/src/main/java/com/example/conwayslife/button_cell.java
@@ -4,17 +4,14 @@ public class button_cell {
     boolean condition;
 
 
-    int id_x;
-    int id_y;
+
     public button_cell() {
         this.condition = false;
-        this.id_x = 0;
-        this.id_y = 0;
+
     }
     public button_cell(boolean condition, int x, int y) {
         this.condition = condition;
-        this.id_x = x;
-        this.id_y = y;
+
     }
 
     public boolean get_condition() {
@@ -25,17 +22,6 @@ public class button_cell {
         this.condition = condition;
     }
 
-    public int[] get_id() {
-        this.id_y = id_x;
-        this.id_y = id_y;
-        int [] id = {id_x, id_y};
-        return id;
-    }
-
-    public void set_id(int id_x, int id_y) {
-        this.id_x = id_x;
-        this.id_y = id_y;
-    }
 
     public void reverse_condition() {
         if (this.condition == true) {
